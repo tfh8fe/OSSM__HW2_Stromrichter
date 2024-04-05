f696164a066b387ae882437f2efdfcb7a1fa73e7
tfh8fe <free1997@gmx.de>
2024-04-05 09:08:46 +0200

    adapt validations

--- test_M3_Stromrichter.py
+++ test_M3_Stromrichter.py
@@ -19,24 +19,24 @@ U_P = 150 # V
 p = 3
 alpha_max = 160
 factor = 3 * np.sqrt(3) / (2*np.pi)
-ue = 1.67
+ue = 1.758
 I_NRMS = 30
 
 def test_calc_norm_u_dc():
-    assert M3_Stromrichter.calc_norm_u_dc(3) == 3 * np.sqrt(3) / (2*np.pi)
-    assert M3_Stromrichter.calc_norm_u_dc(2) == 2 / np.pi
+    assert M3_Stromrichter.calc_norm_u_dc(3) == round(3 * np.sqrt(3) / (2*np.pi),3)
+    assert M3_Stromrichter.calc_norm_u_dc(2) == round(2 / np.pi, 2)
     
 def test_calc_ue():
     assert M3_Stromrichter.calc_ue(400, factor, alpha_max) == 1.758
     assert M3_Stromrichter.calc_ue(120, factor, alpha_max) == 0.528
 
 def test_calc_alpha():
-    assert M3_Stromrichter.calc_alpha(400, factor, 1.758, 150) == 124.32
-    assert M3_Stromrichter.calc_alpha(400, factor, 1.758, 180) == 132.577
+    assert M3_Stromrichter.calc_alpha(400, factor, 1.758, 150) == 124.31
+    assert M3_Stromrichter.calc_alpha(400, factor, 1.758, 180) == 132.564
 
 def test_calc_I_NRMS():
-    assert M3_Stromrichter.calc_I_NRMS(20, U_P, U_NRMS, I_NRMS) == 9.287
-    assert M3_Stromrichter.calc_I_NRMS(40, U_P, U_NRMS, I_NRMS) == 18.573
+    assert M3_Stromrichter.calc_I_NRMS(20, ue) == 9.289
+    assert M3_Stromrichter.calc_I_NRMS(40, ue) == 18.578
 
 def test_calc_lambda():
     assert M3_Stromrichter.calc_lambda(I_P, U_P, U_NRMS, 10) == 0.433
